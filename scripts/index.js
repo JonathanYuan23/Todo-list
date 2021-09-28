82f88bddb38d3dcd56566043ae126b16e827fbc6
Jonathan Yuan <jy90023@gmail.com>
2021-09-28 18:23:32 -0400

    simplify menu icon switching, no longer creates a new svg every time it's switched to x and back. Sidebar is positioned

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,29 @@
-const DOMHandler = (function() {
-
+const DOMHandler = (function () {
     // load svgs
     feather.replace();
 
     // cache DOM
-    const toggleMenu = document.querySelector('.feather-menu');
+    const openMenu = document.querySelector('.feather-menu');
+    const closeMenu = document.querySelector('.feather-x');
+    const sidebar = document.querySelector('#sidebar');
 
     // bind events
-    toggleMenu.addEventListener('click', switchMenuToX);
-    toggleMenu.addEventListener('click', toggleSidebar);
+    openMenu.addEventListener('click', switchMenuToX);
+    closeMenu.addEventListener('click', switchXToMenu);
 
     function switchMenuToX(e) {
-        const parent = e.currentTarget.parentNode;
-        e.currentTarget.remove();
-        // e.currentTarget.removeEventListener('click', switchMenuToX);
-        // toggleMenu.removeEventListener('click', toggleSidebar);
-
-        const x = document.createElement('i');
-        x.setAttribute('data-feather', 'x');
-
-        parent.appendChild(x);
-        feather.replace();
-
-        const toggleX = document.querySelector('.feather-x');
-        toggleX.addEventListener('click', switchXToMenu);
+        openMenu.style.display = 'none';
+        closeMenu.style.display = 'block';
+        toggleSidebar();
     }
 
     function switchXToMenu(e) {
-        const parent = e.currentTarget.parentNode;
-        e.currentTarget.remove();
-        // e.currentTarget.removeEventListener('click', switchXToMenu);
-
-        const menu = document.createElement('i');
-        menu.setAttribute('data-feather', 'menu');
-
-        parent.appendChild(menu);
-        feather.replace();
-
-        const toggleMenu = document.querySelector('.feather-menu');
-        toggleMenu.addEventListener('click', switchMenuToX);
-        toggleMenu.addEventListener('click', toggleSidebar);
+        openMenu.style.display = 'block';
+        closeMenu.style.display = 'none';
+        toggleSidebar();
     }
 
     function toggleSidebar() {
-
+        sidebar.classList.toggle('active');
     }
-
 })();
